8c276289265f4e1e854249dfe4807c4aa9ae8994
Rildoprofessor <125749695+Rildoprofessor@users.noreply.github.com>
2023-04-04 20:42:45 -0300

    Add files via upload

---
+++ processa_formulario.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<?php
+  $nome = $_POST['nome'];
+  $sobrenome = $_POST['sobrenome'];
+
+  echo "Olá, " . $nome . " " . $sobrenome . "! Bem-vindo(a)!";
+?>
+</body>
+</html>
