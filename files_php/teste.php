96f75e7f19004540e5f56a9f4ecce8369230921f
Rildo <rildoprofessor23@gmail.com>
2023-08-05 08:37:41 -0300

    semana 05-08

---
+++ files_php/teste.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo PHP</title>
+</head>
+<body>
+
+<h1>Bem-vindo ao Exemplo PHP!</h1>
+
+<?php
+$str = "Hello world!";
+echo $str;
+?>
+
+</body>
+</html>
+
