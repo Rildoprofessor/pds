e7675ddb9dc73ca5a9e5b4e692a5a3fa827405b9
Rildo <rildoprofessor23@gmail.com>
2023-08-05 21:52:28 -0300

    atividades

---
+++ files_php/semana01/atv2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atividade 02</title>
+</head>
+<body>
+<?php
+    $nome = "João";
+    $sobrenome = "Silva";
+    echo "Nome completo: " . $nome . " " . $sobrenome;
+?>
+</body>
+</html>
---
+++ files_php/semana01/atv3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atividade 03</title>
+</head>
+<body>
+<?php
+    $num1 = 10;
+    $num2 = 5;
+
+    $soma = $num1 + $num2;
+    $subtracao = $num1 - $num2;
+    $multiplicacao = $num1 * $num2;
+    $divisao = $num1 / $num2;
+
+    echo "Soma: " . $soma . "<br>";
+    echo "Subtração: " . $subtracao . "<br>";
+    echo "Multiplicação: " . $multiplicacao . "<br>";
+    echo "Divisão: " . $divisao . "<br>";
+?>
+
+</body>
+</html>
